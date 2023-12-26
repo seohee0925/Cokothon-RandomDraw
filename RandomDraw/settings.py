843aa89a8a1f5627557159cf0e745363d4171377
Junhyeong Seo <seo4306@kookmin.ac.kr>
2023-12-27 05:01:22 +0900

    json-format으로 api 수정

--- RandomDraw/settings.py
+++ RandomDraw/settings.py
@@ -49,8 +49,8 @@ SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'main',
-
+    "corsheaders",
     'rest_framework',
 ]
 
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -79,7 +80,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'RandomDraw.urls'
-ALLOWED_HOSTS = ['192.168.140.251', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
+
 
 TEMPLATES = [
     {
--- accounts/views.py
+++ accounts/views.py
@@ -10,23 +10,48 @@ from .models import Accounts
 
 @csrf_exempt
 def register(request):
-    AccountForm = accountForm(request.POST)
-    AccountForm.save()
+    print(request.body)
+    requestdata = json.loads(request.body.decode('utf-8'))
+    print(requestdata)
+
+    emaildata = requestdata['email'] 
+    namedata = requestdata['name']
+    passworddata = requestdata['password']
+
+
+    data = Accounts(email = emaildata, name = namedata, password = passworddata)
+    data.save()
+    # AccountForm = accountForm(request.POST)
+    # cleaned_data = AccountForm.cleaned_data
+    # form = json.dumps(cleaned_data)
+    # print(form)
+
+    # print(AccountForm.email)
+
+    # AccountForm.fields['email'].initial = emaildata
+    # AccountForm.fields['name'].initial = namedata
+    # AccountForm.fields['password'].initial = passworddata
+
+    # AccountForm.email = emaildata
+    # print(AccountForm.email)
+    # AccountForm.name = namedata
+    # AccountForm.password = passworddata
+
     return JsonResponse({'result' : 'success'}, json_dumps_params={'ensure_ascii': False}, content_type = 'application/json; charest=utf-8')
 
 @csrf_exempt
 def login(request):
-    # # requestdata = json.loads(request.body.decode('utf-8'))
-    # requestdata = json.loads(request.body)
-    # print(requestdata)
+    requestdata = json.loads(request.body.decode('utf-8'))
+    requestdata = json.loads(request.body)
+    print(requestdata)
 
-    emaildata = request.POST.get('email')
-    namedata =  request.POST.get('name')
+    emaildata = requestdata['email']
+    passworddata =  requestdata['password']
 
     request.session['email'] = emaildata
 
     try:
-        account = get_object_or_404(Accounts, email = emaildata, name = namedata)
+        account = get_object_or_404(Accounts, email = emaildata, password = passworddata)
     except:
         return JsonResponse({'result' : "fail"}, json_dumps_params={'ensure_ascii': False}, content_type = 'application/json; charest=utf-8')
     
