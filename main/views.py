18cff4c266674a325b52661619429393050d25f5
윤서희 <appleid21@sookmyung.ac.kr>
2023-12-27 00:37:54 +0900

    Fix: 마이페이지 데이터 합침

--- main/views.py
+++ main/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+
+from accounts.models import Accounts
+from .models import Capsule, picked_capsule
 from .forms import CapsuleCreateForm
-from .models import Capsule
+
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 
 def write_capsule(request):
+    # user_email = Accounts.objects.get(email=email)
+    user_email = request.session['email']
+    user = Accounts.objects.get(email=user_email) # 해당 정보를 가져옴
+
     if request.method == 'POST':
         form = CapsuleCreateForm(request.POST, request.FILES)
         if form.is_valid():
+            # form.instance.email = user_email
             form.save()
+            # return JsonResponse()
             return render(request, 'show_capsule.html')
     else:
         form = CapsuleCreateForm()
-    
     return render(request, 'write_capsule.html', {'form': form})
 
-def show_capsule(request, id):
+def show_capsule(request, id): # 사용자 제외 랜덤
     if request.method == 'GET':
         capsule = get_object_or_404(Capsule, id=id)
 
--- mypage/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import picked_capsule
-admin.site.register(picked_capsule)
